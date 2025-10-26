db0bdb66e9b53efe9ff508d5b0fcfe9ac1b5d08f
Hubert Szelepusta <110931863+Hubi0295@users.noreply.github.com>
2025-10-26 22:27:23 +0100

    Modify cron job schedule and enhance logging
    Updated cron job timing and improved output messages.

--- init_and_backup.sh
+++ init_and_backup.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
-
-# Instalacja niezbędnych narzędzi
 apt-get update -qq && apt-get install -y -qq cron postgresql-client curl
-
 export PGPASSWORD=inzynierka
-until pg_isready -h warehouse-db -p 5432 -U postgres; do sleep 3; done
-
 PRODUCT_COUNT=$(psql -h warehouse-db -U postgres -d warehouse -tAc "SELECT COUNT(*) FROM products;")
-
 if [ "$PRODUCT_COUNT" = "0" ]; then
+  echo "🌱 Table 'products' is empty — seeding with init_data.sql"
   psql -h warehouse-db -U postgres -d warehouse -f /init_data.sql
 else
-  echo "Table 'products' already contains data"
+  echo "✅ Table 'products' already contains data — skipping seed."
 fi
-echo "50 15 * * * /bin/bash /db_sync.sh >> /var/log/db_sync.log 2>&1" > /etc/cron.d/db-sync
+
+echo "05 21 * * * /bin/bash /db_sync.sh >> /var/log/db_sync.log 2>&1" > /etc/cron.d/db-sync
 chmod 0644 /etc/cron.d/db-sync
 crontab /etc/cron.d/db-sync
+echo "🚀 Starting cron daemon..."
 cron -f
